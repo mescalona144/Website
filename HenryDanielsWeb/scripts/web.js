e2e652022350c64673adc4c7bda39d692cf9291e
Miguel Escalona <80832402+mescalona144@users.noreply.github.com>
2021-07-26 11:55:38 -0300

    Add files via upload
    El sitio web "Henry Daniels Motorcycle" es una tienda online que está en construcción. Sus imágenes y textos son hasta ahora de carácter ilustrativo. Está siendo diseñado con HTML, CSS y Javascript. Tiene por ahora un menú interactivo y el sitio web va a contener un carrito de compras y un PHP que permita interactuar por medio del e-mail.

---
+++ HenryDanielsWeb/scripts/web.js
@@ -0,0 +1,55 @@
+/* Variables**/ 
+let btnMenu = document.getElementById('btn-menu');
+let mainNav = document.getElementById('main-nav');
+btnMenu.addEventListener('click', function(){
+    mainNav.classList.toggle('mostrar');
+} )
+ /* Slider */
+ const slider = document.querySelector("#slider");
+ let sliderSection = document.querySelectorAll(".slider__section");
+ let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+ const btnLeft = document.querySelector("#btn-left");
+ const btnRight = document.querySelector("#btn-right");
+
+ slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+
+ /* Funciones */
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 400);
+}
+
+function Prev() {
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft = "0"
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none"
+        slider.insertAdjacentElement("afterbegin", sliderSectionLast)
+        slider.style.marginLeft = "-100%"
+    },400);
+}
+
+ /* Botones */
+btnRight.addEventListener("click", function(){
+    Next();
+});
+
+btnLeft.addEventListener("click", function(){
+    Prev();
+});
+
+
+/* Slider Automático */
+setInterval(function(){
+    Next();
+}, 3000);
